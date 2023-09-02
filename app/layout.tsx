e5526dc60a984d969e9fabc541beada4b99548db
TheVy <ntvy2k@gmail.com>
2023-09-02 23:23:02 +0700

    vi -> en

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="vi">
+    <html lang="en">
       <body className={inter.className}>
         <div className="w-full h-full relative">
           <Sidebar />
